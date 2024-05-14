bfe8103cb8bfdec5ea0e9a23915df5500813c319
SplendidY <splendidyyy@gmail.com>
2024-05-14 21:23:48 +0800

    cesium add

--- frontend/src/components/Service.vue
+++ frontend/src/components/Service.vue
@@ -1,16 +1,47 @@
-<script setup>
-</script>
-
 <template>
-  <div>
-    <el-button type="primary" round>primary</el-button>
-    <el-button type="success" round>success</el-button>
-    <el-button type="info" round>info</el-button>
-    <el-button type="warning" round>warning</el-button>
-    <el-button type="danger" round>danger</el-button>
-  </div>
+  <el-container style="height: 98.34vh;">
+
+    <el-header style="background-color: #008997; color: #fff; text-align: center; line-height: 60px;">Service Header</el-header>
+
+    <el-container>
+      <el-aside width="200px" style="background-color: #008997;">
+        <el-menu default-active="1-1" class="el-menu-vertical-demo" text-color="#000000" active-text-color="#008997">
+          <el-submenu index="1">
+            <el-menu-item index="1-1">1</el-menu-item>
+            <el-menu-item index="1-2">2</el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <el-menu-item index="2-1">1</el-menu-item>
+            <el-menu-item index="2-2">2</el-menu-item>
+          </el-submenu>
+          <el-submenu index="3">
+            <el-menu-item index="3-1">1</el-menu-item>
+            <el-menu-item index="3-2">2</el-menu-item>
+          </el-submenu>
+          <el-submenu index="4">
+            <el-menu-item index="4-1">1</el-menu-item>
+            <el-menu-item index="4-2">2</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      
+      <el-main style="background-color: #E4E7ED; padding: 20px;">
+        <Cesium></Cesium>
+      </el-main>
+      
+    </el-container>
+  </el-container>
 </template>
 
-<style scoped>
+<script setup>
+  import Cesium from './Cesium.vue'
+</script>
 
+<style scoped>
+.el-menu-vertical-demo {
+  width: 100%;
+}
+.el-main {
+  position: relative;
+}
 </style>
---
+++ frontend/src/jses/init.js
@@ -0,0 +1,54 @@
+let viewer;
+
+function init() {
+  Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0Y2U5MWRhOC0zYzdhLTRjMGItODkwNC02NzVmNGFmMTBkOWEiLCJpZCI6MjA1MjM5LCJpYXQiOjE3MTE2ODUwMjF9.RRfIFU8B-huDx7VQOLeAmMabtoIcIkA1m2SRaRYopUI';
+  viewer = new Cesium.Viewer('cesiumContainer',{
+  animation: false,
+  timeline: false,
+  baseLayerPicker: false,
+  fullscreenButton: false,
+  geocoder: false,
+  homeButton: false,
+  infoBox: false,
+  sceneModePicker: false,
+  selectionIndicator: false,
+  navigationHelpButton: false,
+  navigationInstructionsInitiallyVisible: false,
+  sceneMode: Cesium.SceneMode.SCENE3D
+});
+  viewer._cesiumWidget._creditContainer.style.display = "none";
+  // window.viewer = viewer;
+  viewer.camera.setView({
+            destination: Cesium.Cartesian3.fromDegrees(120, 30, 15000000),
+            orientation: {
+            heading : Cesium.Math.toRadians(348.4202942851978),
+            pitch : Cesium.Math.toRadians(-89.74026687972041),
+            roll : Cesium.Math.toRadians(0)
+        },
+        complete:function callback() {
+        }
+        }); 
+  Cesium.CesiumTerrainProvider.fromIonAssetId(1, {
+    requestWaterMask:true,
+    requestVertexNormals:true,
+  }).then((terrainProvider) => {
+    viewer.terrainProvider = terrainProvider;
+  }).catch((e) => {
+    console.log(e);
+  });  
+  var gaodeProvider = new Cesium.UrlTemplateImageryProvider({
+    url: 'https://webst02.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}',
+    credit: '高德地图',
+    tilingScheme: new Cesium.WebMercatorTilingScheme(),
+    maximumLevel: 18,
+  });
+  viewer.imageryLayers.addImageryProvider(gaodeProvider);
+  // provide('viewer', viewer);
+}
+function getViewer() {
+  return viewer;
+}
+export {
+    init,
+    getViewer
+}
